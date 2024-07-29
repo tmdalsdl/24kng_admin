2b2aa52293b807ed400b10e08f4ef8723719fbd2
tmdalsdl <lsm123452002@gmail.com>
2024-07-29 17:44:55 +0900

    update

--- videoupload/urls.py
+++ videoupload/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import admin_page, video_upload, dashboard
+from .views import admin_page, video_upload, get_public_ip  # dashboard 삭제
 
 urlpatterns = [
     path('adminpage/', admin_page, name='admin_page'),
     path('upload/', video_upload, name='video_upload'),
-    path('dashboard/', dashboard, name='dashboard'),
+    path('get_public_ip/', get_public_ip, name='get_public_ip'),  # 추가된 부분
 ]
