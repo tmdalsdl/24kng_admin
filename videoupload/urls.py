cf217d6a8b262a4b21e3ea418a7709261faef711
tmdalsdl <lsm123452002@gmail.com>
2024-07-30 10:50:14 +0900

    수정

--- videoupload/urls.py
+++ videoupload/urls.py
@@ -4,5 +4,4 @@ from .views import admin_page, video_upload, dashboard
 urlpatterns = [
     path('adminpage/', admin_page, name='admin_page'),
     path('upload/', video_upload, name='video_upload'),
-    path('dashboard/', dashboard, name='dashboard'),
 ]
